78a005aa3902749fe3e2e63c60006f3f6b508af7
Frank <fr.heggeness@gmail.com>
2019-05-09 14:31:19 -1000

    css list page

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 module.exports = {
   client: 'postgresql',
   connection: {
+    host: process.env.POSTGRES_HOST,
     database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
